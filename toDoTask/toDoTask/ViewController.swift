af9fbca8b6fbcfab789e09c1e18f04b55c66d682
kim33zy <kayde348@gmail.com>
2021-01-01 16:56:02 +0900

    First screen and connect each items in first screen

--- toDoTask/toDoTask/ViewController.swift
+++ toDoTask/toDoTask/ViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+//Connect each items for design
+    @IBOutlet weak var firstScreenImageView: UIImageView!
+    @IBOutlet weak var firstScreenLabel: UILabel!
+    @IBOutlet weak var firstScreenImageViewRight: UIImageView!
+    @IBOutlet weak var firstScreenInstructionLabel: UILabel!
+    @IBOutlet weak var firstScreenDecisionButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    //Center Label Design
+        firstScreenLabel.text = "Hello"
+        firstScreenLabel.textAlignment = .center
+        firstScreenLabel.textColor = .black
+        firstScreenLabel.adjustsFontSizeToFitWidth = true
+        firstScreenLabel.font = UIFont(name: "Gill Sans", size: 35)
+        
+    //Instruction Label Design
+        firstScreenInstructionLabel.numberOfLines = 0
+        firstScreenInstructionLabel.lineBreakMode = .byWordWrapping
+        firstScreenInstructionLabel.text = "Choose uo to 5 lists. You can change them and add more later."
+        firstScreenInstructionLabel.textAlignment = .center
+        firstScreenInstructionLabel.font = UIFont(name: "Gill Sans", size: 22)
+        firstScreenInstructionLabel.textColor = .darkGray
+        
+    //Decision Button Design
+        firstScreenDecisionButton.setTitle("Done", for: .normal)
+        firstScreenDecisionButton.backgroundColor = .black
+        firstScreenDecisionButton.setTitleColor(.white, for: .normal)
+        firstScreenDecisionButton.layer.cornerRadius = 10
+        firstScreenDecisionButton.layer.borderWidth = 1
+        firstScreenDecisionButton.layer.borderColor = UIColor.black.cgColor
+        firstScreenDecisionButton.clipsToBounds = true
+        firstScreenDecisionButton.titleLabel?.font = UIFont(name: "Gill Sans", size: 22)
+        
+        
     }
 
 
